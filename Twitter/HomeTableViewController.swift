b935eb90ca981d990e7dfbd060e229df683f5b1e
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-02-19 15:15:09 -0500

    Create and design tweet prototype table view cell

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -21,16 +21,23 @@ class HomeTableViewController: UITableViewController {
         UserDefaults.standard.setValue(false, forKey: "userLoggedIn")
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath) as! TweetTableViewCell
+        
+        cell.userNameLabel.text = "Name"
+        cell.tweetContent.text = "Something"
+        
+        return cell
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
     
 }
